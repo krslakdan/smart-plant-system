0cadac9240cc7b2b4b5b6627ee652abb420068cd
krslakdan <191279454+krslakdan@users.noreply.github.com>
2025-12-18 21:16:29 +0100

    Implement offline sensors logic and use if block instead of ngIf

--- frontend/plantSystem/src/app/app.ts
+++ frontend/plantSystem/src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal, OnInit, PLATFORM_ID, inject } from '@angular/core';
-import {isPlatformBrowser, NgClass, NgIf} from '@angular/common';
+import {DatePipe, isPlatformBrowser, NgClass} from '@angular/common';
 import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, onValue, set } from 'firebase/database';
 
 @Component({
   selector: 'app-root',
-  imports: [NgClass, NgIf],
+  imports: [NgClass, DatePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -16,8 +16,26 @@ export class App implements OnInit {
   protected readonly co = signal(0);
   protected readonly nh3 = signal(0);
   protected readonly ch4 = signal(0);
+
+  protected readonly lastDataTimestamp = signal<number>(Date.now());
+  protected readonly sensorOnline = signal<boolean>(false);
+  private offlineTimer: any;
+  private readonly OFFLINE_TIMEOUT = 10000;
+
   private platformId = inject(PLATFORM_ID);
   private database: any;
+
+  private dataReceived() {
+    this.lastDataTimestamp.set(Date.now());
+    this.sensorOnline.set(true);
+
+    clearTimeout(this.offlineTimer);
+    this.offlineTimer = setTimeout(() => {
+      this.sensorOnline.set(false);
+    }, this.OFFLINE_TIMEOUT);
+  }
+
+
 
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
@@ -32,28 +50,43 @@ export class App implements OnInit {
       onValue(temperatureRef, (snapshot) => {
         const data = snapshot.val();
         this.temperature.set(data);
+        this.dataReceived();
       });
 
       const ledRef = ref(this.database, 'isLEDon');
       onValue(ledRef, (snapshot) => {
         const data = snapshot.val();
         this.isLEDon.set(data);
+        this.dataReceived();
       });
 
       const moistureRef = ref(this.database, 'soilMoisture');
       onValue(moistureRef, (snapshot) => {
         const data = snapshot.val();
         this.soilMoisture.set(data);
+        this.dataReceived();
       });
 
       const airQualityRef = ref(this.database, 'CO');
-      onValue(airQualityRef, snapshot => { this.co.set(snapshot.val()); });
+      onValue(airQualityRef, snapshot => {
+        const data = snapshot.val();
+        this.co.set(data);
+        this.dataReceived();
+      });
 
       const nh3Ref = ref(this.database, 'NH3');
-      onValue(nh3Ref, snapshot => { this.nh3.set(snapshot.val()); });
+      onValue(nh3Ref, snapshot => {
+        const data = snapshot.val();
+        this.nh3.set(data);
+        this.dataReceived();
+      });
 
       const ch4Ref = ref(this.database, 'CH4');
-      onValue(ch4Ref, snapshot => { this.ch4.set(snapshot.val()); });
+      onValue(ch4Ref, snapshot => {
+        const data = snapshot.val();
+        this.ch4.set(data);
+        this.dataReceived();
+      });
     }
   }
 
